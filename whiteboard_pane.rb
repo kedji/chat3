cb8cfd1812b58f036886d2417845075be63d6f9a
kedji <kedjikeen@yahoo.com>
2010-02-08 23:15:27 -0500

    Whiteboard demo available; not yet integrated.

--- whiteboard_pane.rb
+++ whiteboard_pane.rb
@@ -6,6 +6,13 @@
 # In no event will the authors be held liable for damages arising from the
 # use of this software.  All license is reserved.
 
+####  Whiteboard Protocol  ####
+# Whiteboard messages are a space-delimited set of commands.  Commands are
+# made up of a type-byte and then a comma-delimited set of values.  As an
+# example, here's a message containing two line commands:
+# "L10,20,15,25 L15,20,10,25"
+# The 'L' character specifies that each command is a line, and the LINE
+# command takes four integer values - x1,y1,x2,y2.
 
 require 'rubygems' rescue nil
 require 'fox16'
@@ -14,15 +21,25 @@ require 'fox16/colors'
 include Fox
 
 class WhiteboardPane < FXPacker
-  BOARD_WIDTH      = 480
-  BOARD_HEIGHT     = 320
+  BOARD_WIDTH      = 480     # Initial window width
+  BOARD_HEIGHT     = 320     # Initial window height
+  IMG_WIDTH        = 1920    # Maximum width of our canvas
+  IMG_HEIGHT       = 1080    # Maximum height of our canvas
   BUTTON_WIDTH     = 70
-  FRONT_COLOR       = 0xffffffff
-  BACK_COLOR      = 0xfff0000f
+  BACK_COLOR       = 0xffffffff
+  FRONT_COLOR      = 0xfff0000f
+  BUFFER_TIME      = 1.1     # Seconds for which we buffer
 
   def initialize(parent, skin = {})
     super(parent, :opts => LAYOUT_FILL)
     @mouse_down = false
+    @cmds = []
+    @cmd_time = nil
+    @colors = { 'R' => 0xff0000ff,
+                'B' => 0xfff0000f,
+                'G' => 0xff00aa00,
+                'D' => 0xff000000 }
+    @color = 'D'
 
     # We may want a 2-pixel border on the inside-top of this pane, otherwise
     # no borders.
@@ -36,26 +53,36 @@ class WhiteboardPane < FXPacker
       LAYOUT_SIDE_RIGHT | LAYOUT_FIX_WIDTH, :width => BUTTON_WIDTH)
     button_clear = FXButton.new(button_list, "Clear", :opts => FRAME_RAISED |
       FRAME_THICK | LAYOUT_FILL_X | LAYOUT_SIDE_TOP)
-#    button_clear.connect(SEL_COMMAND) { clear_board }
+    button_clear.connect(SEL_COMMAND) { clear_board }
+
+    # Now draw the color buttons
+    @colors.each do |c,v|
+      button = FXButton.new(button_list, " ", :opts => FRAME_RAISED |
+        FRAME_THICK | LAYOUT_FILL_X | LAYOUT_SIDE_TOP)
+      button.connect(SEL_COMMAND) { @color = c }
+      button.backColor = v
+    end
 
     # Drawing area goes on the left
-    @canvas = FXCanvas.new(frames, :opts => LAYOUT_FILL_X | LAYOUT_FILL_Y |
-      LAYOUT_TOP | LAYOUT_LEFT)
+    cframe = FXHorizontalFrame.new(frames, :opts => LAYOUT_FILL | FRAME_SUNKEN |
+      FRAME_THICK | LAYOUT_SIDE_LEFT, :padLeft => 0, :padRight => 0,
+      :padTop => 0, :padBottom => 0, :hSpacing => 0, :vSpacing => 0)
+    @canvas = FXCanvas.new(cframe, :opts => LAYOUT_FILL)
     @canvas.connect(SEL_PAINT, method(:board_draw))
     @canvas.connect(SEL_LEFTBUTTONPRESS, method(:left_button_down))
     @canvas.connect(SEL_LEFTBUTTONRELEASE, method(:left_button_up))
     @canvas.connect(SEL_MOTION, method(:mouse_motion))
 
     # Backup of the canvas on which we're drawing
-    @image = FXImage.new(app, :width => BOARD_WIDTH, :height => BOARD_HEIGHT)
+    @image = FXImage.new(app, :width => 1920, :height => 1080)
     @image.create
     FXDCWindow.new(@image) do |dc|
       dc.foreground = BACK_COLOR
-      dc.fillRectangle(0, 0, BOARD_WIDTH, BOARD_HEIGHT)
+      dc.fillRectangle(0, 0, 1920, 1080)
     end
   end
 
-  # Redraw the whole board?  Nah, just the parts that weren't previously there.
+  # Redraw the whole board?  Restore an image?
   def board_draw(*params)
     FXDCWindow.new(@canvas) do |dc|
       dc.drawImage(@image, 0, 0)
@@ -64,11 +91,11 @@ class WhiteboardPane < FXPacker
 
   # Clear the whole board (local only)
   def clear_board
-Kernel.exit
-    FXDCWindow.new(@canvas) do |dc|
+    FXDCWindow.new(@image) do |dc|
       dc.foreground = BACK_COLOR
-      dc.fillRectangle(0, 0, @prior_width, @prior_height)
+      dc.fillRectangle(0, 0, IMG_WIDTH, IMG_HEIGHT)
     end
+    board_draw
   end
 
   # This object generates only one synthetic event - character strings
@@ -87,20 +114,58 @@ Kernel.exit
   def left_button_up(sender, selector, event)
     @canvas.ungrab
     @mouse_down = false
+    flush_commands
     board_draw
   end
 
   # The mouse has moved.  Do something if the button is down
   def mouse_motion(sender, selector, event)
     return nil unless @mouse_down
-@on_line_block.call("LN#{event.last_x},#{event.last_y},#{event.win_x},#{event.win_y}")
-#    FXDCWindow.new(@canvas, event) do |dc|
-    FXDCWindow.new(@image, event) do |dc|
-      dc.foreground = FRONT_COLOR
+
+    # Draw the temporary line just for immediate user feedback
+    FXDCWindow.new(@canvas) do |dc|
+      dc.foreground = @colors[@color]
       dc.drawLine(event.last_x, event.last_y, event.win_x, event.win_y)
-      dc.drawLine(10, 10, event.win_x, event.win_y)
     end
+
+    # Draw the permanent line to our buffer
+    FXDCWindow.new(@image) do |dc|
+      dc.foreground = @colors[@color]
+      dc.drawLine(event.last_x, event.last_y, event.win_x, event.win_y)
+    end
+
+    # Now send the line to the room
+    buffer_command("L,#{@color},#{event.last_x},#{event.last_y}" +
+                   ",#{event.win_x},#{event.win_y}")
     true
+  end
+
+  # Buffer commands so we can send them in chunks
+  def buffer_command(cmd)
+    @cmd_time ||= Time.now.to_f
+    @cmds << cmd
+    flush_commands if Time.now.to_f - @cmd_time > BUFFER_TIME
+  end
+
+  # Send all the buffered commands, if any
+  def flush_commands
+    @cmd_time = nil
+    @on_line_block.call(@cmds.join(' ')) unless @cmds.empty?
+    @cmds = []
+  end
+
+  # We've gotten a list of drawing commands from the room.  Let's draw them!
+  def disp(text)
+    FXDCWindow.new(@image) do |dc|
+      text.split(' ').each do |cmd|
+        cmd = cmd.split(',')
+        if cmd.first == 'L'
+          dc.foreground = @colors[cmd[1]].to_i
+          dc.drawLine(cmd[2].to_i, cmd[3].to_i + 1, cmd[4].to_i, cmd[5].to_i + 1)
+        end
+      end
+    end
+    board_draw
   end
 
 end  # of class WhiteboardPane
@@ -113,7 +178,7 @@ if __FILE__ == $0
       :width => WhiteboardPane::BOARD_WIDTH,
       :height => WhiteboardPane::BOARD_HEIGHT)
     board = WhiteboardPane.new(main)
-    board.on_line { |x| puts x }
+    board.on_line { |x| board.disp(x) ; puts x }
     main.show
     app.create
     app.run
